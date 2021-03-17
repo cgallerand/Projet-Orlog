e0481836ffd467bc3c8f412410ae5d83d9fc8cc3
Christophe Gallerand <christophe.gallerand@soprasteria.com>
2021-03-17 17:55:09 +0100

    gitflow-feature-stash: mapping

--- src/main/java/metier/Utilisateur.java
+++ src/main/java/metier/Utilisateur.java
@@ -1,15 +1,29 @@
 package metier;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("utilisateur")
 public class Utilisateur extends Compte {
+	
+	private Integer level;
 
 	public Utilisateur() {
 	}
 
-	public Utilisateur(int id, String login, String password,int level) {
-		super(id,login,password,level);
+	public Utilisateur(int id, String login, String password, int level) {
+		super(id,login,password);
+		this.level=level;
 	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(Integer level) {
+		this.level = level;
+	}
+	
 
 }
